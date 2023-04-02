e2e19118a0fca5f4230c718fb0b762b65fcf5d89
BeerMonsterDota <obelikc17@gmail.com>
2023-04-03 01:02:22 +0300

    new file:   .gitignore 	new file:   config.json 	new file:   forge.config.js 	new file:   package-lock.json 	new file:   package.json 	new file:   src/index.js 	new file:   yarn.lock

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron')
+const config = require('./../config.json')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 2560,
+    height: 1440,
+    transparent: true,
+    frame: false,
+    resizable: false,
+    alwaysOnTop: true,
+    // fullscreen & kiosk - позволяют скрыть таскбар
+    fullscreen: true,
+    kiosk: true,
+    x: 0,
+    y: 0,
+  })
+  win.setIgnoreMouseEvents(true)
+  win.setAlwaysOnTop(true, 'screen-saver')
+  win.setVisibleOnAllWorkspaces(true)
+  win.setMenuBarVisibility(false)
+
+  win.loadURL(config.url)
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
